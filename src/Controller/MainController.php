1e6ac92cb8cfe98b500a3c1d39ccc8a1c26a0f39
Losbang <contact@arthurmichel.com>
2019-08-19 18:34:37 +0200

    Correction MainController & index template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,12 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
 }
